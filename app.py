f107e329a5bc075b37192fcf271bd532640a0040
Prathik <99598276+PRATHIKGIT@users.noreply.github.com>
2023-09-21 20:27:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,75 @@
 import nltk
-nltk.download('popular')
+nltk.download('popular')#  Natural Language Toolkit (NLTK) library installed will download and
+# install a collection of popular NLTK data packages, including corpora,
+# models etc
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
 from keras.models import load_model
-from flask import Flask, render_template, request, jsonify
+model = load_model('model.h5')
 import json
 import random
-
-app = Flask(__name__)
-app.static_folder = 'static'
-
-model = load_model('model.h5')
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('texts.pkl','rb'))
 classes = pickle.load(open('labels.pkl','rb'))
-
-# Function to process the incoming message and generate a response
-def chatbot_response(msg):
-    print("chatbot is analysing")
-    sentence_words = nltk.word_tokenize(msg)
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-    bag = [0]*len(words)
+    return sentence_words
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
     for s in sentence_words:
-        for i, w in enumerate(words):
-            if w == s:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
-    p = bow(msg, words, show_details=False)
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    ERROR_THRESHOLD = 0.7
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    tag = return_list[0]['intent']
-    list_of_intents = intents['intents']
+    return return_list
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
     for i in list_of_intents:
-        if i['tag'] == tag:
+        if(i['tag']== tag):
             result = random.choice(i['responses'])
             break
     return result
-
-@app.route("/", methods=["GET", "POST"])
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    if ints:
+       res = getResponse(ints, intents)
+       return res
+    else :
+        return  "I'm sorry, but I couldn't understand your query.Try asking the query in another way.Ask queries related to mental health"
+from flask import Flask, render_template, request
+app = Flask(__name__)
+app.static_folder = 'static'
+@app.route("/")
 def home():
-    print("home visited")
     return render_template("index.html")
-
-@app.route("/get", methods=["GET", "POST"])
+@app.route("/get")
 def get_bot_response():
-    print("message received by bot")
-    if request.method == "POST":
-        userText = request.form["msg"]
-    else:  # GET request
-        userText = request.args.get("msg")
+    userText = request.args.get('msg')
     return chatbot_response(userText)
-
 if __name__ == "__main__":
-    print("main entered")
     app.run()
 
